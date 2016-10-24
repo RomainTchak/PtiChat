2dbd388558709382e2ab22ba165bc1d251db25d5
RomainTchak <romain.tchakamian@student.ecp.fr>
2016-10-24 11:38:16 +0200

    tout le package

---
+++ WpfApplication2Solution/WpfApplication2/Client.cs
@@ -0,0 +1,54 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication2
+{
+    public class Client
+    {
+        
+
+        public void OnNewConnectedCustomer(string name)
+        {
+            UserEventArgs UserEA = new UserEventArgs(name);
+            NewConnectedCustomer?.Invoke(this, UserEA);
+        }
+
+        public event EventHandler<UserEventArgs> NewConnectedCustomer;
+
+        public sealed class UserEventArgs : EventArgs
+        {
+            public string UserName { get; private set; }
+
+            public UserEventArgs (string username)
+            {
+                UserName = username;
+            }
+        }
+        
+
+        public void OnReceivedMessage ()
+        {
+            MessageEventArgs MessageEA = new MessageEventArgs("Bonjour je suis un message test");
+            ReceivedMessage?.Invoke(this, MessageEA);
+        }
+
+        public event EventHandler<MessageEventArgs> ReceivedMessage;
+
+        public sealed class MessageEventArgs : EventArgs
+        {
+            public string MessageContent { get; private set; }
+
+            public MessageEventArgs(string messageContent)
+            {
+                MessageContent = messageContent;
+            }
+        }
+
+    }
+}
---
+++ WpfApplication2Solution/WpfApplication2/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication2
+{
+    public class Message
+    {
+        public string Body { get; set; }
+        public string Sender { get; set; }
+    }
+}
